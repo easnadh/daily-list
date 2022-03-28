87c3f3b4c93dab4f7981d9c6c3467ba9c723cfeb
jjcmerin <91774742+jjcmerin@users.noreply.github.com>
2022-03-28 18:02:03 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
 
+let arr = [{
+	id: 0,
+	text: "покормить кота",
+	isDone: false
+}];
 
 function addElem(event) {
 	event.preventDefault();
-
-	const lb = document.getElementById("label");
-	const ol = document.getElementById("list");
-	let children = ol.children.length + 1;
-	const li = document.createElement("li");
-	li.innerHTML = `<input type=\"checkbox\" class="list-element">
-	<span>${lb.value}</span>
-	<button onclick="delElem()">x</button>`;
-	li.setAttribute("id", "element" + children);
-	ol.appendChild(li);
-	lb.value = "";
+	const label = document.getElementById("label");
+	const id = arr.length;
+	arr.push({
+		id,
+		text: label.value,
+		isDone: false
+	});
+	console.log(arr);
+	render();
+	localStorage.setItem('to-do-list', JSON.stringify(arr));
+	label.value = "";
 }
 
 function delElem(ev) {
-	const innerEvent = ev || event;
-	innerEvent.target.parentElement.remove();
+	// const innerEvent = ev || event;
+	// innerEvent.target.parentElement.remove();
+	render();
 }
+
+function render() {
+	const ol = document.getElementById("list");
+	ol.innerHTML = "";
+	for(let item of arr) {
+
+		let li = document.createElement("li");
+		li.innerHTML = `<input type="checkbox" onclick="checkboxCheck"><span>${item.text}</span>
+		<button onclick="delElem()">x</button>`;
+		// li.setAttribute("id", "element" + item.id);
+		ol.appendChild(li);
+	}
+}
+
+// function checkboxCheck() {
+// 	let checkbox = document.querySelectorAll("input[type=checkbox]");
+
+// 	for (let i = 0; i < checkbox.length; i++) {
+// 		checkbox[i].onchange = function () {
+// 			arr = JSON.parse(localStorage.getItem("to-do-list"));
+// 			arr[i].check = this.checked;
+// 			localStorage.setItem("to-do-list", JSON.stringify(arr));
+// 		}
+// 	}
+// }
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("form").addEventListener("submit", addElem);
-});
 
+	if (localStorage.getItem("to-do-list") != undefined) {
+		arr = JSON.parse(localStorage.getItem("to-do-list"));
+	}
+
+	render();
+	
+});
 
